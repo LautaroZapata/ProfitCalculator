b98bc09d0cc517df5b4f5fedeaabea9ab851031f
Lautaro Zapata <oratualzc@gmail.com>
2024-06-25 11:52:02 -0300

    arreglo

--- index.js
+++ index.js
@@ -8,15 +8,27 @@
 // console.log();
 // document.write(`VALOR TOTAL PESOS ${valorpeso}`)
 
-let input = document.getElementById('precioSTOCKX');
 let btn = document.getElementById('btnSubmit');
+let porcentaje;
 let precioFinalProfit = document.getElementById('precioFinalProfit')
 
 function calcular () {
-    let precioUsa = parseInt(input.value) + 26;
-    let precioTotalProfit = precioUsa * 1.40;
-    let precioPesos = precioTotalProfit * 39.5;
-    precioFinalProfit.innerHTML = `VALOR TOTAL PESOS 40% <b class="totalPesos">$${Math.round(precioPesos)}</b> VALOR TOTAL EN DOLARES = <b class="totalPesos">$${precioTotalProfit}`;
+    let precioUsa = parseInt(document.getElementById('precioSTOCKX').value) + 26;
+
+    let profit = document.getElementById('cantProfit').value;
+
+    
+    if(profit == '30') {
+        porcentaje = 1.30
+    }else if(profit == '40'){
+        porcentaje = 1.40
+    }else {
+        porcentaje = 1.50
+    }
+    let profitDolar = precioUsa * porcentaje;
+    let precioPesos = profitDolar * 39.5;
+    precioFinalProfit.innerHTML = `VALOR TOTAL PESOS %${profit} PROFIT <b class="totalPesos">$${Math.round(precioPesos)}</b> VALOR TOTAL EN DOLARES = <b class="totalPesos">$${Math.round(profitDolar)}`;
+    console.log(profit);
 }
 
 
